3732fe4ada348de1048bba145860cf651bc6be00
Victor Ferraz de Castro <victordecastro23@gmail.com>
2023-08-23 17:59:07 -0300

    relacao entre trafego e velocidade

--- semaforo_inteligente/Radar/radar2.py
+++ semaforo_inteligente/Radar/radar2.py
@@ -34,8 +34,13 @@ class Radar(object):
     
     def publish_vel(self):
         while(True):
-            velocity = round(random.uniform(5.0, 70.0), 2)
-            num_cars = random.randint(1, 100)
+            num_cars = round(random.gauss(mu = 50, sigma = 20))
+            num_cars = num_cars if num_cars > 1 else 1
+
+            traffic_factor = (-1) * ((num_cars - 50) * 2) / 5
+            
+            velocity = round(random.gauss(mu = 40 + traffic_factor, sigma = 15), 2)
+            velocity =  velocity if velocity > 5 else 5
             msg = {"street": self.__id, "cars": num_cars, "mean velocity": velocity, "time": str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))}
             self.log(msg)
             self.__client.publish(self.__topic, json.dumps(msg))
